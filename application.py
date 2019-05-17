eda07fa4593d5b9b1405bc14ae4266db04c8aa13
jake <jnschaible@gmail.com>
2019-05-17 00:07:13 -0400

    implemented customer ordering and delivery

--- application.py
+++ application.py
@@ -12,12 +12,17 @@ from flask_login import login_user, logout_user, current_user
 from flask_login import login_required, LoginManager
 from werkzeug.urls import url_parse
 from forms import LoginForm, RegistrationForm
-import json
-
+import json, urllib2, datetime
 
 app = Flask(__name__)
 login = LoginManager(app)
 login.login_view = 'show_login'
+
+
+DELIVERY_FEE = 2.99
+RESTAURANT_ADDRESS = '13020 Livingston Rd, Naples, FL 34105'
+APP_KEY = 'AIzaSyAlmemzptHQOxsvsq3H_jlTGrM4A1nLnMQ'
+MAX_DELIVERY_DISTANCE = 32187  # Distance in meters, roughly equals 20 miles
 
 
 def connect():
@@ -38,16 +43,6 @@ def load_user(user_id):
     session = connect()
     user = session.query(User).filter_by(id=user_id).one_or_none()
     return user
-
-
-def get_user_id(email):
-    """ Get user ID matching given email address"""
-    session = connect()
-    user = session.query(User).filter_by(email=email).one_or_none()
-    if user is None:
-        return None
-    else:
-        return user.id
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -192,7 +187,7 @@ def show_cart():
     for item in items:
         subtotal += float(item.price) * item.quantity
     if subtotal > 0:
-        fee = 2.99
+        fee = DELIVERY_FEE
     else:
         fee = 0
     tax = (subtotal + fee) * 0.07
@@ -201,8 +196,144 @@ def show_cart():
     fee = "{0:.2f}".format(fee)
     tax = "{0:.2f}".format(tax)
     total = "{0:.2f}".format(total)
+    delivery_time = str(get_delivery_time()/60) + ' minutes'
     return render_template('cart.html', items=items, subtotal=subtotal,
-                           fee=fee, tax=tax, total=total)
+                           fee=fee, tax=tax, total=total, user=current_user,
+                           delivery_time=delivery_time)
+
+
+@app.route('/cart/update_address', methods=['GET', 'POST'])
+@login_required
+def update_address():
+    """ Update the user's address"""
+    session = connect()
+    user = load_user(current_user.id)
+    if request.method == 'POST':
+        if request.form['address']:
+            address = request.form['address']
+            if validate_address(address) is False:
+                flash("Address is invalid or outside delivery radius!")
+                return redirect(url_for('show_cart'))
+            user.address = address
+            user = session.merge(user)
+            flash("Address saved!")
+            session.commit()
+    return redirect(url_for('show_cart'))
+
+
+def validate_address(address):
+    """ Validates the address string
+        Returns true if valid, false if not
+    """
+    # User has no address saved
+    if address is None:
+        return False
+    # User is outside delivery radius
+    if get_travel_distance(address) > MAX_DELIVERY_DISTANCE:
+        return False
+    # If none of the above cases returned false, the address is okay
+    return True
+
+
+@app.route('/cart/order_placed')
+@login_required
+def place_order():
+    """ Place order for delivery for the items currently in the user's cart"""
+    session = connect()
+    try:
+        user_id = current_user.id
+    except AttributeError:
+        return "Error getting user ID"
+    # Query for cart contents
+    items = session.query(Cart).filter_by(user_id=user_id).all()
+    # Redirect user if no items in order
+    if not items:
+        flash("No items in order!")
+        return redirect(url_for('show_cart'))
+    # Create new entry in order table
+    order_time = datetime.datetime.now()
+    delivery_time = order_time + datetime.timedelta(0,get_delivery_time())
+    order = Order(user_id=user_id, order_time=order_time,
+                  delivery_time=delivery_time)
+    session.add(order)
+    # Add each item to order_item table and remove from cart
+    for i in items:
+        order_item = OrderItem(order_id=order.id, menu_item_id=i.menu_item_id,
+                               quantity=i.quantity)
+        session.add(order_item)
+        session.delete(i)
+    session.commit()
+    delivery_time = str(get_delivery_time()/60) + ' minutes'
+    # Form URL for delivery map
+    origin = RESTAURANT_ADDRESS.replace(' ', '+')
+    destination = current_user.address
+    if validate_address(destination) is False:
+        flash("Address is invalid or outside delivery radius!")
+        return redirect(url_for('show_cart'))
+    destination = destination.replace(' ', '+')
+    destination = destination.replace('\r\n', '+')
+    destination = destination.replace('\r', '+')
+    destination = destination.replace('\n', '+')
+    map_url = 'https://www.google.com/maps/embed/v1/directions?origin='
+    map_url += origin
+    map_url += '&destination='
+    map_url += destination
+    map_url += '&key='
+    map_url += APP_KEY
+    return render_template('orderComplete.html', delivery_time=delivery_time,
+                           map_url=map_url)
+
+
+######################
+# Delivery Functions #
+######################
+def get_travel_data(destination):
+    """ Returns JSON travel data"""
+    origin = RESTAURANT_ADDRESS.replace(' ', '+')
+    destination = destination.replace(' ', '+')
+    destination = destination.replace('\r\n', '+')
+    destination = destination.replace('\r', '+')
+    destination = destination.replace('\n', '+')
+    url = 'https://maps.googleapis.com/maps/api/directions/json?origin='
+    url += origin
+    url += '&destination='
+    url += destination
+    url += '&mode=driving&key='
+    url += APP_KEY
+    travel_data = json.load(urllib2.urlopen(url))
+    return travel_data
+
+def get_travel_time(destination):
+    """ Returns travel time in seconds between restaurant
+        and destination for delivery
+    """
+    travel_data = get_travel_data(destination)
+    return travel_data['routes'][0]['legs'][0]['duration']['value']
+
+
+def get_travel_distance(destination):
+    """ Returns travel distance in meters between restaurant
+        and destination for delivery
+    """
+    travel_data = get_travel_data(destination)
+    return travel_data['routes'][0]['legs'][0]['distance']['value']
+
+
+def get_prep_time():
+    #TODO
+    return 1200
+
+
+def get_delivery_time():
+    """ The delivery time is the combination of 
+        the prep time and the travel time
+    """
+    try:
+        delivery_time = get_travel_time(current_user.address)
+        delivery_time += get_prep_time()
+        return delivery_time
+    except AttributeError:
+        return "Error getting user address"
 
 
 #######################
